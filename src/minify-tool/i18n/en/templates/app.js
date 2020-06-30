ff8dd57fb8bd039048a9469496051a31409c04c7
MattIPv4 <me@mattcowley.co.uk>
2020-06-30 16:07:27 +0100

    Initial tool base, empty

---
+++ src/minify-tool/i18n/en/templates/app.js
@@ -0,0 +1,4 @@
+export default {
+    title: 'Minify Tool',
+    oss: 'This tool is {link|open-source on GitHub|https://github.com/do-community/minify-tool} under the {link|Apache-2.0|https://github.com/do-community/minify-tool/blob/master/LICENSE} license! We welcome feedback and contributions.',
+};
