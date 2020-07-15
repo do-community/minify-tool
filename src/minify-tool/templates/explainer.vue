0fa248ebd3d93eea0c23f73a4b2693b933f93dd6
MattIPv4 <me@mattcowley.co.uk>
2020-07-15 15:52:11 +0100

    Add text for why minify

--- src/minify-tool/templates/explainer.vue
+++ src/minify-tool/templates/explainer.vue
@@ -1,7 +1,25 @@
 <template>
     <div class="explainer">
         <h3>Why minify your JavaScript?</h3>
-        <!-- TODO -->
+
+        <p>
+            Minifying, or minification, is where you remove unnecessary characters from your code, whether they might be
+            whitespace (such as indentation), code that isn't ever used, comments in your code or long names for
+            variables that can be replaced with something shorter.
+        </p>
+
+        <p>
+            Minification of your code results in it taking up less space, making it faster to send from a server to a
+            client, as well as using less bandwidth in doing so. This improves the user experience on your site as it
+            can load faster.
+        </p>
+
+        <p>
+            You should only minify the code that you are distributing though, not your source version that you are
+            writing, as minified code is harder to read and understand, making debugging more complicated. Providing a
+            source map helps with this, as it maps the minified code back to the original source code, allowing
+            production errors to be mapped to the correct bit of code in the source version.
+        </p>
 
         <h3>Using Terser in a production pipeline</h3>
         <!-- TODO -->
